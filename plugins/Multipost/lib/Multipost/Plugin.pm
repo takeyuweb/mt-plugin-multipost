48d39fbd1bb685dbee4aac26bc6649b5e6e0c0ef
Yuichi Takeuchi <uzuki05@takeyu-web.com>
2012-06-28 11:52:05 +0900

    権限チェック

--- plugins/Multipost/lib/Multipost/Plugin.pm
+++ plugins/Multipost/lib/Multipost/Plugin.pm
@@ -25,7 +25,9 @@ sub _cb_tp_edit_entry {
     my ( $cb, $app, $param, $tmpl ) = @_;
 
     my $blog = $app->blog;
+    my $user = $app->user;
 
+    return unless defined( $user );
     return unless defined( $blog );
     return unless $app->param( '_type' ) eq 'entry';
 
@@ -37,9 +39,14 @@ sub _cb_tp_edit_entry {
             label => $plugin->translate( 'Entry Copies' ),
         });
 
-    my @blogs = MT->model( 'blog' )->load(
+    my @blogs = ();
+    my $blog_iter = MT->model( 'blog' )->load_iter(
         { id => { 'not' => $blog->id } }
     );
+    while ( my $b = $blog_iter->() ) {
+        next unless _can_on_blog( $user, $b, 'create_post' );
+        push @blogs, $b;
+    }
     return unless scalar( @blogs ) > 0;
 
     $param->{ multipost } = $app->param( 'multipost' );
@@ -172,6 +179,8 @@ sub _cb_ps_entry {
     my $target_blog = MT->model( 'blog' )->load( $target_blog_id );
     return 1 unless defined($target_blog) && $target_blog->id != $entry->blog_id;
 
+    return 1 unless _can_on_blog( $app->user, $target_blog, 'create_post' );
+
     local $app->{_blog} = $target_blog;
 
     my $target_category;
@@ -188,32 +197,36 @@ sub _cb_ps_entry {
     $new_entry->category_id( undef );
     $new_entry->modified_on( undef );
     $new_entry->created_on( undef );
+    $new_entry->status( MT::Entry::HOLD() )
+      unless _can_on_blog( $app->user, $target_blog, 'publish_post' );
     
     my @asset_ids = split( ',', $app->param('include_asset_ids') || '' );
     my @new_assets = ();
-    foreach my $asset_id ( @asset_ids ) {
-        my $asset = MT->model( 'asset' )->load( $asset_id );
-        next unless $asset;
-        my $new_asset = $asset->clone;
-        $new_asset->id( undef );
-        $new_asset->blog_id( $target_blog->id );
-        $new_asset->modified_on( undef );
-        $new_asset->created_on( undef );
-
-        require MT::FileMgr;
-        my $fmgr = MT::FileMgr->new( 'Local' ) or die MT::FileMgr->errstr;;
-        my $dir = dirname( $new_asset->file_path );
-        $fmgr->mkpath( $dir ) or return MT->trans_error( "Error making path '[_1]': [_2]",
-                                                         $new_asset->file_path, $fmgr->errstr );
-        $fmgr->put_data( $fmgr->get_data( $asset->file_path ), $new_asset->file_path, 'upload' );
-        
-        if ( $new_asset->save ) {
-            push @new_assets, $new_asset;
-
-            foreach my $key ( qw( text text_more ) ) {
-                my $text = $new_entry->$key;
-                $text =~ s/@{[ $asset->url ]}/@{[ $new_asset->url ]}/g;
-                $new_entry->$key( $text );
+    if ( _can_on_blog( $app->user, $target_blog, 'upload' ) ) {
+        foreach my $asset_id ( @asset_ids ) {
+            my $asset = MT->model( 'asset' )->load( $asset_id );
+            next unless $asset;
+            my $new_asset = $asset->clone;
+            $new_asset->id( undef );
+            $new_asset->blog_id( $target_blog->id );
+            $new_asset->modified_on( undef );
+            $new_asset->created_on( undef );
+            
+            require MT::FileMgr;
+            my $fmgr = MT::FileMgr->new( 'Local' ) or die MT::FileMgr->errstr;;
+            my $dir = dirname( $new_asset->file_path );
+            $fmgr->mkpath( $dir ) or return MT->trans_error( "Error making path '[_1]': [_2]",
+                                                             $new_asset->file_path, $fmgr->errstr );
+            $fmgr->put_data( $fmgr->get_data( $asset->file_path ), $new_asset->file_path, 'upload' );
+            
+            if ( $new_asset->save ) {
+                push @new_assets, $new_asset;
+                
+                foreach my $key ( qw( text text_more ) ) {
+                    my $text = $new_entry->$key;
+                    $text =~ s/@{[ $asset->url ]}/@{[ $new_asset->url ]}/g;
+                    $new_entry->$key( $text );
+                }
             }
         }
     }
@@ -276,6 +289,41 @@ sub _rebuild_entry {
     );
 
     $ret;
+}
+
+
+
+sub _can_administer_blog {
+    my ( $user, $blog ) = @_;
+
+    if ( $blog && ( ref $blog ne 'MT::Blog' ) ) {
+        $blog = undef;
+    }
+    return 0 unless $blog;
+
+    return 1 if $user->is_superuser;
+
+    return 0 unless my $perm = $user->permissions( $blog->id );
+
+    #return 1 if $perm->can_administer_website;
+    return 1 if $perm->can_administer_blog;
+
+    return 0;
+}
+
+# 指定のブログで指定の操作が可能か
+# _can_on_blog( $user, $blog, 'create_post' );
+sub _can_on_blog {
+    my ( $user, $blog, $action ) = @_;
+    return 0 unless $blog;
+    return 0 unless $user;
+    return 1 if _can_administer_blog( $user, $blog );
+
+    my $perms = MT::Permission->load({
+        blog_id => $blog->id,
+        author_id => $user->id});
+    my $can_action = "can_$action";
+    $perms && $perms->$can_action();
 }
 
 1;
